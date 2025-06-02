62f66e7fb5f8701a2c00b00e0497930a65b8f3fc
klasix12 <sunlightpattern@yandex.ru>
2025-06-03 00:19:01 +0300

    сделал приложение клиентом OAuth 2

--- src/main/java/com/klasix12/config/SecurityConfig.java
+++ src/main/java/com/klasix12/config/SecurityConfig.java
@@ -1,46 +1,60 @@
 package com.klasix12.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-    @Value("${client-id}")
-    private String clientId;
-
-    @Value("${client-secret}")
-    private String clientSecret;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.oauth2Login(Customizer.withDefaults());
+        // делаем приложение клиентом OAuth 2
+        http.oauth2Client(Customizer.withDefaults());
 
-        http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
+        http.authorizeHttpRequests(c -> c.anyRequest().permitAll());
         return http.build();
     }
 
     @Bean
     public ClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryClientRegistrationRepository(this.googleClientRegistration());
+        ClientRegistration c1 =
+                ClientRegistration.withRegistrationId("1")
+                        .clientId("client")
+                        .clientSecret("secret")
+                        .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                        .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                        .tokenUri("http://localhost:9090/oauth2/token")
+                        .scope(OidcScopes.OPENID)
+                        .build();
+        return new InMemoryClientRegistrationRepository(c1);
     }
 
-    /*
-    Подсос с конфига это временное решение, так он должен дергать данные из бд, иначе нет смысла делать так, если все
-    равно придется пересобирать приложение после изменения конфигурации
-     */
-    private ClientRegistration googleClientRegistration() {
-        return CommonOAuth2Provider.GOOGLE.getBuilder("google")
-                .clientId(clientId)
-                .clientSecret(clientSecret)
+    @Bean
+    public OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository
+    ) {
+        OAuth2AuthorizedClientProvider provider = OAuth2AuthorizedClientProviderBuilder.builder()
+                .clientCredentials()
                 .build();
+        DefaultOAuth2AuthorizedClientManager clientManager = new DefaultOAuth2AuthorizedClientManager(
+                clientRegistrationRepository, authorizedClientRepository
+        );
+        clientManager.setAuthorizedClientProvider(provider);
+        return clientManager;
     }
 }
--- src/main/java/com/klasix12/controller/HomeController.java
+++ src/main/java/com/klasix12/controller/HomeController.java
@@ -1,15 +1,24 @@
 package com.klasix12.controller;
 
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.stereotype.Controller;
+import lombok.AllArgsConstructor;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@AllArgsConstructor
 public class HomeController {
 
+    private final OAuth2AuthorizedClientManager manager;
+
     @GetMapping("/home")
-    public String home(OAuth2AuthenticationToken authentication) {
-        System.out.println(authentication);
-        return "main.html";
+    public OAuth2AuthorizedClient home() {
+        OAuth2AuthorizeRequest req = OAuth2AuthorizeRequest
+                .withClientRegistrationId("1")
+                .principal("client")
+                .build();
+        return manager.authorize(req);
     }
 }
